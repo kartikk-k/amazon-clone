f34160d604be61e0beaae1d19a8dbbdd6deb85a2
Kartik Khorwal <88406357+kartikk-k@users.noreply.github.com>
2023-02-13 02:42:45 +0530

    update firebase

--- src/firebase/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBFcSZS80txWgCx7kp8_xpQQ_RAGYClUF4",
-    authDomain: "clone-19e10.firebaseapp.com",
-    projectId: "clone-19e10",
-    storageBucket: "clone-19e10.appspot.com",
-    messagingSenderId: "1067130524067",
-    appId: "1:1067130524067:web:61262f0ee0e445ddfd0a16",
-    measurementId: "G-SSRBC7QJLV"
-};
